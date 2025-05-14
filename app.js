4e353bb161f018dcd6c86bf92ce2a53471e6fa16
Nicolas <razachai@outlook.com>
2025-05-14 14:55:47 -0300

    utilizando funções com e sem parametro

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
- let titulo = document.querySelector('h1');
- // document.qS seleciona o titulo que iremos alterar no html
- titulo.innerHTML = 'Jogo do número secreto';
- // inner nos deixa introduzir o texto dentro do html
+//  let titulo = document.querySelector('h1');
+//  // document.qS seleciona o titulo que iremos alterar no html
+//  titulo.innerHTML = 'Jogo do número secreto';
+//  // inner nos deixa introduzir o texto dentro do html
 
- let paragrafo = document.querySelector('p');
- paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//  let paragrafo = document.querySelector('p');
+//  paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+ function exibirTexto(tag, texto) {
+        let campo = document.querySelector(tag);
+        campo.innerHTML = texto;
+
+ }
+ exibirTexto('h1','Jogo do número secreto');
+ exibirTexto('p','Escolha um número entre 1 e 10');
+
+  function verificarChute() {
+        console.log('o botão foi clicado');
+
+  }
+
+ // criando a  função de clicar no botão, sempre entre chaves
