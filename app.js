c4dd69ea48b940423a5592617cc7c245692c1607
Nicolas <razachai@outlook.com>
2025-05-14 14:13:20 -0300

    alterando titulo e paragrafo

---
+++ app.js
@@ -0,0 +1,7 @@
+ let titulo = document.querySelector('h1');
+ // document.qS seleciona o titulo que iremos alterar no html
+ titulo.innerHTML = 'Jogo do número secreto';
+ // inner nos deixa introduzir o texto dentro do html
+
+ let paragrafo = document.querySelector('p');
+ paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
