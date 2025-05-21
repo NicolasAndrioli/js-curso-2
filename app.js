d8ce02bb23be382582ad061d5db5ef517a20217d
Nicolas <razachai@outlook.com>
2025-05-21 16:42:37 -0300

    função de reiniciar jogo criada

--- app.js
+++ app.js
@@ -1,61 +1,76 @@
- //let titulo = document.querySelector('h1');
+//let titulo = document.querySelector('h1');
 //  document.qS seleciona o titulo que iremos alterar no html
- //titulo.innerHTML = 'Jogo do número secreto';
+//titulo.innerHTML = 'Jogo do número secreto';
 //  // inner nos deixa introduzir o texto dentro do html
 
 //  let paragrafo = document.querySelector('p');
 //  paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-    let randomNumber = gerarNumero();
+let randomNumber = gerarNumero();
 
-    let tentativas = 1;
+let tentativas = 1;
 
- function exibirTexto(tag, texto) {
-        let campo = document.querySelector(tag);
-        campo.innerHTML = texto;
+exibirMensagemInicial();
 
- }
- exibirTexto('h1','Jogo do número secreto');
- exibirTexto('p','Escolha um número entre 1 e 10');
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 
-  function verificarChute() {
-        let chute = document.querySelector('input').value;
-        console.log('o botão foi clicado');
+}
 
-        if (chute == randomNumber){
-            exibirTexto('h1','Acertou!!')
-            let palavraTentativa  = tentativas > 1 ? 'tentativas ' : 'tentativa';
+function exibirMensagemInicial() {
+    exibirTexto('h1', 'Jogo do número secreto');
+    exibirTexto('p', 'Escolha um número entre 1 e 10');
 
-            let mensagemTentativa = `Você acertou o número secreto com ${tentativas} ${palavraTentativa}! `;
+}
 
-            exibirTexto('p',mensagemTentativa);
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log('o botão foi clicado');
 
-            document.getElementById('reiniciar').removeAttribute('disabled');
-            //pegar um campo do html pelo id 
+    if (chute == randomNumber) {
+        exibirTexto('h1', 'Acertou!!')
+        let palavraTentativa = tentativas > 1 ? 'tentativas ' : 'tentativa';
 
-            
+        let mensagemTentativa = `Você acertou o número secreto com ${tentativas} ${palavraTentativa}! `;
 
+        exibirTexto('p', mensagemTentativa);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        //pegar um campo do html pelo id 
+
+
+
+    } else {
+        if (chute > randomNumber) {
+            exibirTexto('p', 'O número secreto é menor');
         } else {
-            if (chute > randomNumber){
-                exibirTexto('p','O número secreto é menor');
-            } else {
-                exibirTexto('p','O número secreto é maior');
-            }
-            tentativas++;
-            limparCampo()
+            exibirTexto('p', 'O número secreto é maior');
         }
-  }
+        tentativas++;
+        limparCampo()
+    }
+}
 
-  function gerarNumero(){
-       return parseInt(Math.random() * 10 + 1);
-  }
+function gerarNumero() {
+    return parseInt(Math.random() * 10 + 1);
+}
 
-  function limparCampo(){
-        chute = document.querySelector('input');
-        chute.value = '';
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
 
-  }
+}
 
-  
+function reiniciarJogo() {
+    randomNumber = gerarNumero();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 
- // criando a  função de clicar no botão, sempre entre chaves
+}
+
+
+
+// criando a  função de clicar no botão, sempre entre chaves
